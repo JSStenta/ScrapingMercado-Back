834b9856ad3df0938e98b4f0d04c2e2a6348c1b5
Juansezhin <jsstenta@outlook.com>
2024-10-27 21:58:05 -0300

    Coto funciona flama

--- scraper.ts
+++ scraper.ts
@@ -17,9 +17,13 @@ export async function scrapeProductPrices(
     let results: ProductInfo[] = [];
     for (const supermarket of supermarkets) {
         switch (supermarket) {
-            case 'algo':
-                const cotoResults = await scrapeCoto(product); // Espera el resultado de scrapeCoto
-                results.push(...cotoResults); // Combina los resultados de scrapeCoto en el array de resultados
+            case 'coto':
+                const cotoResults = await scrapeCoto(product);
+                results.push(...cotoResults);
+                break;
+            case 'carrefour':
+                const carrefourResults = await scrapeCarrefour(product);
+                results.push(...carrefourResults);
                 break;
             default:
                 // Simulando el scraping y devolviendo un objeto de ejemplo.
@@ -53,9 +57,18 @@ export async function scrapeCoto(search: string): Promise<ProductInfo[]> {
     try {
         await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
         await page.setExtraHTTPHeaders({
-            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Language": "es-Ar,es;q=0.9",
         });
-        await page.goto(url);
+        await page.goto(url, { waitUntil: 'load' });
+        await page.evaluate(() => {
+            const menor = Array.from(document.querySelectorAll('option')).find(option => option.textContent.trim() === 'Precio: de menor a mayor');
+            if (menor) {
+                menor.selected = true; // Selecciona la opción en el DOM
+                menor.parentElement.dispatchEvent(new Event('change')); // Dispara el evento "change"
+            }
+        });
+        await page.waitForNavigation();
+        
         const products = await page.evaluate(() => {
             return Array.from(document.querySelectorAll(".leftList")).map((product) => ({
                 supermarket: 'Coto',
@@ -85,4 +98,80 @@ export async function scrapeCoto(search: string): Promise<ProductInfo[]> {
     }
     await browser.close();
     return results;
+}
+
+export async function scrapeCarrefour(search: string) {
+    const browser = await puppeteer.launch({
+        executablePath: 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe',
+        headless: false,
+        slowMo: 200,
+    });
+    const results: ProductInfo[] = [];
+    const page = await browser.newPage();
+    const url = `https://www.carrefour.com.ar/${encodeURIComponent(search)}?_q=${encodeURIComponent(search)}&map=ft&order=OrderByPriceASC`;
+    console.log(url);
+
+    try {
+        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        await page.setExtraHTTPHeaders({
+            "Accept-Language": "es-Ar,es;q=0.9",
+        });
+        await page.goto(url);
+
+        const products = await page.evaluate(() => {
+            console.log('entro al evaluate');
+            return Array.from(document.querySelectorAll('a article')).map((product) => ({
+                supermarket: 'Carrefour',
+                title:
+                    product.querySelector(".t-body")?.textContent ||
+                    "No encontrado",
+                /*
+                unit: //Arreglar
+                    product.querySelector(".unit")?.textContent || "Unidad no encontrada",*/
+                price: //Arreglar
+                    Array.from(product.querySelectorAll('.valtech-carrefourar-product-price-0-x-currencyContainer span')).map(span => span.textContent.trim()).join('') ||
+                    "Precio no encontrado",
+                image:
+                    product.querySelector("img")?.getAttribute('src') || "Imagen no encontrada",
+                /*
+                link: //Arreglar
+                    "https://www.cotodigital3.com.ar" +
+                    product.querySelector("a")?.getAttribute("href") ||
+                    "Link no encontrado",*/
+            }));
+        });
+        results.push(...products);
+    } catch (error) {
+        console.error(`Error en el scraping de Coto: ${error}`);
+    } finally {
+        await page.close();
+    }
+    await browser.close();
+    return results;
+}
+
+export async function scrapeTest(search: string) {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 500,
+        executablePath: 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
+    });
+    const results: ProductInfo[] = [];
+    const page = await browser.newPage();
+    let url = `https://www.cotodigital3.com.ar/`;
+
+    // Capturamos la URL del redireccionamiento
+    page.on('response', response => {
+        if (response.status() === 302 || response.status() === 301) {  // Detecta códigos de redirección
+            url = response.headers().location;
+        }
+    });
+    await page.goto(url);
+    await page.close();
+    await browser.close();
+
+
+
+    console.log(baseURI);
+
 }
--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 //script.js
-document.getElementById("searchForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const product = document.getElementById("product").value;
-  const supermarkets = Array.from(
-    document.querySelectorAll('input[name="supermarkets"]:checked')
-  ).map((cb) => cb.value);
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  const response = await fetch("/search", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ product, supermarkets }),
+    const product = document.getElementById("product").value;
+    const checkboxes = document.querySelectorAll(
+      'input[name="supermarkets"]:checked'
+    );
+    const supermarkets = Array.from(checkboxes).map(
+      (checkbox) => checkbox.value
+    );
+
+    try {
+      const response = await fetch("http://localhost:8000/search", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ product, supermarkets }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error en la búsqueda de productos");
+      }
+
+      const data = await response.json();
+      populateTable(data);
+    } catch (error) {
+      console.error(error);
+    }
   });
 
-  const results = await response.json();
-  const resultsBody = document.getElementById("resultsBody");
-  resultsBody.innerHTML = "";
-  results.forEach((result) => {
+function populateTable(data) {
+  const tbody = document.querySelector("#resultsTable tbody");
+  tbody.innerHTML = ""; // Limpia los resultados anteriores
+
+  data.forEach((item) => {
     const row = document.createElement("tr");
+
     row.innerHTML = `
-            <td>${result.supermarket}</td>
-            <td>${result.title}</td>
-            <td>${result.price}</td>
-            <td>${result.unit}</td>
-            <td><img src='${result.image}'></td>
-            <td><a href='${result.link}'>Enlace al producto</td>
+            <td>${item.supermarket}</td>
+            <td>${item.title}</td>
+            <td>${item.price}</td>
+            <td>${item.unit}</td>
+            <td><img src="${item.image}" alt="${item.product}" width="50" /></td>
+            <td><a href="${item.link}" target="_blank">Ver producto</a></td>
         `;
-    resultsBody.appendChild(row);
+
+    tbody.appendChild(row);
   });
-});
+}
